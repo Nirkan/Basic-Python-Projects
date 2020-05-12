55cea3aa483d06a1cdf4e9ed20422a030ec55550
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-05-12 22:52:33 +0200

    Prime number

---
+++ Prime-number/Prime_number.py
@@ -0,0 +1,13 @@
+# Print Prime numbers
+
+start = int(input('Enter the starting number of the desired range : '))
+end  =  int(input("Enter the ending number of the desired range :"))
+
+for val in range(start, end + 1):
+	if val > 1:
+		for n in range(2, val//2 + 2):
+			if (val % n) == 0 : 
+				break 
+			else: 
+				if n == val // 2 + 1:
+					print(val)
