3647359c883873def0f45fd4ac2b5746b7c73964
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-06-12 17:32:20 +0200

    basic mad lib generator

---
+++ Mad_lib/Mad_lib.py
@@ -0,0 +1,25 @@
+
+print("Roses are _____.")
+print("The violet's _____")
+print("____ is sweet")
+print("And so are you.")
+
+print("*")
+print("*")
+print("Fill in the blanks in the poem above :")
+print("*")
+print("*")
+
+color1 = input("Enter a color : ")
+
+color2 = input("Enter another color : ")
+
+Edible_item = input("Enter an edible_item : ")
+
+print("*")
+print("*")
+
+print("Roses are " + color1)
+print("The violet's " + color2)
+print( Edible_item + " is sweet")
+print("And so are you")
