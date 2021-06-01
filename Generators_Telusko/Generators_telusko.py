0cf0fb44983054ca894887f349d635bd93a38a9b
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2021-06-01 16:56:11 +0200

    simple generator example

---
+++ Generators_Telusko/Generators_telusko.py
@@ -0,0 +1,22 @@
+
+def topten():
+	
+	n = 1
+	
+	while n <= 10:
+		sq = n*n
+		yield sq
+		n += 1
+
+
+
+values = topten()
+
+
+for i in values:
+	print(i)
+
+
+
+## Generator saves the memory and cpu use.
+## It is faster 
